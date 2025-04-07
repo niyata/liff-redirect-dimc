62cab945000760522ca9c7abd086f71d2c6b7f6c
iWorn Studio <niyata2206@gmail.com>
2025-04-07 22:10:21 +0900

    Delete env.js

--- env.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-  LIFF_ID: '1656178796-37apgwOv',
-  API_URL: 'https://script.google.com/macros/s/AKfycbwX6nCzIVbczSJPpA6jjpRHiirL8NVuoVd3tOral0C_JrzmSRkH6-nicCCBlqXH4hEWjg/exec'
-};
