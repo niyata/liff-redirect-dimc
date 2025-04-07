cfcd5d4f32aede71982f3dc5deaae6b650890b49
iWorn Studio <niyata2206@gmail.com>
2025-04-07 21:15:21 +0900

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.ENV = {
   LIFF_ID: '1656178796-37apgwOv',
-  API_URL: 'https://script.google.com/macros/s/AKfycbykWc2UFzee_v-YoFQsjfNKgL3Wgtwf188WjN7XwbhQt8QaoK0Ie4vHk4x_dfjd336eLQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbxlK3uG_NQoeJaeB-mAlUZ9hAllGiRM3OKiuiP2dD5Y7pn7BGIJQ-zcS8tTLu3TCm16gg/exec'
 };
