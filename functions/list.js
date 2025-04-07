86a3604b8ffc0cd435b2392d6ec72f3fc750b5f4
iWorn Studio <niyata2206@gmail.com>
2025-04-07 22:26:44 +0900

    Add files via upload

--- functions/list.js
+++ functions/list.js
@@ -3,17 +3,23 @@ const axios = require("axios");
 
 exports.handler = async () => {
   try {
-    const res = await axios.get(`${process.env.XATA_DB_URL}/tables/links/query`, {
+    const res = await axios.post(`${process.env.XATA_DB_URL}/tables/links/query`, {
+      columns: ["id", "name", "url"]
+    }, {
       headers: {
         Authorization: `Bearer ${process.env.XATA_API_KEY}`,
         "Content-Type": "application/json"
-      },
-      data: {
-        columns: ["id", "name", "url"]
       }
     });
-    return { statusCode: 200, body: JSON.stringify(res.data.records) };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.data.records)
+    };
   } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message, detail: err.response?.data })
+    };
   }
 };
