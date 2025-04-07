562de82b833efe49cf0ea6711bbd58e1ed18d5d2
iWorn Studio <niyata2206@gmail.com>
2025-04-07 20:41:44 +0900

    Create env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+// env.js
+window.ENV = {
+  LIFF_ID: '1656178796-37apgwOv',
+  API_URL: 'https://script.google.com/macros/s/AKfycbykWc2UFzee_v-YoFQsjfNKgL3Wgtwf188WjN7XwbhQt8QaoK0Ie4vHk4x_dfjd336eLQ/exec'
+};
